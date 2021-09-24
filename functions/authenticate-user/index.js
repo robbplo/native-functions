1cb628637612e956f109c816b04ed9e73da210bf
Thomas van Ophem <thomas.van.ophem@bettyblocks.com>
2021-09-24 07:52:38 +0200

    feat: add custom (record based) authentication

--- functions/authenticate-user/index.js
+++ functions/authenticate-user/index.js
@@ -1,20 +1,33 @@
-const authenticateUser = async ({
-  authenticationProfile: { id: authenticationProfileId },
-  username,
-  password,
-}) => {
-  const mutation = `
-    mutation($username: String, $password: String, $authenticationProfileId: String) {
+const USERNAME_PASSWORD = `
+    mutation($username: String!, $password: String!, $authenticationProfileId: String!) {
       login(username: $username, password: $password, authProfileUuid: $authenticationProfileId) {
         jwtToken
       }
     }
   `;
+const CUSTOM_AUTHENTICATION = `
+    mutation($userId: Int!, , $authenticationProfileId: String!) {
+      generateJwt(userId: $userId, authenticationProfileId: $authenticationProfileId) {
+        jwtToken
+      }
+    }
+  `;
+
+const authenticateUser = async ({
+  authenticationProfile: { id: authenticationProfileId, kind },
+  username,
+  password,
+  record,
+}) => {
+  const mutation =
+    kind === 'username_password' ? USERNAME_PASSWORD : CUSTOM_AUTHENTICATION;
+
+  const userData =
+    kind === 'username_password' ? { username, password } : { userId: record };
 
   const input = {
-    username,
-    password,
     authenticationProfileId,
+    ...userData,
   };
 
   const { data, errors } = await gql(mutation, input);
