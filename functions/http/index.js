a17708ad56fb5e0018a61a9fc899cf1a8456ea65
Stefan van Delft <stefan.van.delft@bettyblocks.com>
2021-10-05 14:30:40 +0200

    chore(ACTIONS-79): Spread http logic into small functions.

--- functions/http/index.js
+++ functions/http/index.js
@@ -1,34 +1,36 @@
-const http = async ({
-  url,
-  method,
-  body,
-  headers,
-  protocol,
-  queryParameters,
-}) => {
-  const queryString = queryParameters
+const parseHeaders = (headers) =>
+  headers.reduce((acc, { key, value }) => ({ ...acc, [key]: value }), {});
+
+const parseQueryParameters = (queryParameters) =>
+  queryParameters
     .map(({ key, value }, index) => {
       const paramKey = index === 0 ? `?${key}` : key;
       return `${paramKey}=${encodeURIComponent(value)}`;
     })
     .join('&');
-  const fetchUrl = `${protocol}://${url}${queryString}`;
-  const fetchHeaders = headers.reduce(
-    (acc, { key, value }) => ({ ...acc, [key]: value }),
-    {},
-  );
 
-  const response = await fetch(fetchUrl, {
+const generateUrl = (url, protocol, queryParameters) =>
+  `${protocol}://${url}${parseQueryParameters(queryParameters)}`;
+
+const http = async ({
+  url,
+  method,
+  body,
+  headers = [],
+  protocol,
+  queryParameters = [],
+}) => {
+  const fetchUrl = generateUrl(url, protocol, queryParameters);
+  const options = {
     method,
-    headers: fetchHeaders,
+    headers: parseHeaders(headers),
     ...(method !== 'get' && { body }),
-  });
+  };
 
+  const response = await fetch(fetchUrl, options);
   const data = await response.text();
 
-  return {
-    as: data,
-  };
+  return { as: data };
 };
 
 export default http;
