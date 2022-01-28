ae187c294d3d93c9c31282d8059af5af639fc259
Thomas van Ophem <thomas.van.ophem@bettyblocks.com>
2022-01-28 08:40:52 +0100

    chore: fix linter issues

--- functions/loop/index.js
+++ functions/loop/index.js
@@ -1,5 +1,6 @@
+/* eslint-disable no-await-in-loop */
 const loop = async ({ collection: { data: collection } }, steps) => {
-  for (let index = 0; index < collection.length; index++) {
+  for (let index = 0; index < collection.length; index += 1) {
     await steps({ iterator: collection[index] });
   }
 };
