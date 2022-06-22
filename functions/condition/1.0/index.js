abe0c3a9aa3300730e7e643b017ed112232b05a8
Justin van der Velden <justin.van.der.velden@bettyblocks.com>
2022-06-22 14:11:43 +0200

    feat: add native condition function

--- functions/condition/1.0/index.js
+++ functions/condition/1.0/index.js
@@ -1,11 +1,11 @@
-const condition = async ({ value }, steps) => {
-  const result = !!value;
-
-  if (result) {
-    await steps();
+const condition = async (_, branches) => {
+  for await (let {result, steps} of branches()) {
+    if (result) {
+      await steps();
+      return {result};
+    }
   }
-
-  return result;
+  return {result: false};
 };
 
 export default condition;
