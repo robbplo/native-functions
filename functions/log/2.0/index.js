d8db908cfd7ecf1656bd8b3a92dcbdfbe8a5cb89
ihadjo <hajokoerts@hotmail.com>
2022-06-24 13:56:34 +0200

    chore: remove if severitytype
    Remove if severitytype

--- functions/log/2.0/index.js
+++ functions/log/2.0/index.js
@@ -1,12 +1,8 @@
 const log = async ({ severity, variables }) => {
-  const severityTypes = ['info', 'debug', 'warn', 'error'];
-
-  if (severityTypes.includes(severity)) {
-    variables.forEach((variable) => {
-      // eslint-disable-next-line no-console
-      console[severity](`${variable.key} : ${JSON.stringify(variable.value)}`);
-    });
-  }
+  variables.forEach((variable) => {
+    // eslint-disable-next-line no-console
+    console[severity](`${variable.key} : ${JSON.stringify(variable.value)}`);
+  });
 };
 
 export default log;
